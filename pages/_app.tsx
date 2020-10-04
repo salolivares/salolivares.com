67b512a5f253b70202d5ba74ed9a2794f7428c45
Sal Olivares <git@salolivares.com>
2020-10-04 13:28:41 -0700

    feat: add tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {AppProps} from 'next/app';
+
+import '../styles/index.css';
+
+export default function App({Component, pageProps}: AppProps){
+    return <Component {...pageProps} />
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  variants: {},
+  plugins: [],
+}
