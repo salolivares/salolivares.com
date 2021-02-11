5828d821dd0a7d69bba899ae361debd13c9687b8
Sal Olivares <git@salolivares.com>
2021-02-10 21:51:56 -0800

    update tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
-import '../styles/index.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
