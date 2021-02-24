1b81aa6c431c0fc4408e6793396dc98a78dc6fd2
Sal Olivares <git@salolivares.com>
2021-02-23 20:37:57 -0800

    no more photo mode

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,16 +1,14 @@
 /* eslint-disable react/require-default-props */
 import React, { ReactNode, useEffect, useState } from 'react';
 import Link from 'next/link';
-import clsx from 'clsx';
 import { useTheme } from 'next-themes';
-import Head from './Head';
+import Head from 'next/head';
 import { ThemeToggleButton } from './ThemeToggleButton';
 
 type WrapperProps = {
   children?: ReactNode;
   title?: string;
   description?: string;
-  photoMode?: boolean;
 };
 
 const navLinks = [
@@ -19,7 +17,7 @@ const navLinks = [
   { link: '/photos', name: 'Photos' },
 ];
 
-const Container = ({ children, title = 'Sal Olivares', description = '', photoMode = false }: WrapperProps) => {
+const Container = ({ children, title = 'Sal Olivares', description = '' }: WrapperProps) => {
   const [isMounted, setIsMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -35,25 +33,22 @@ const Container = ({ children, title = 'Sal Olivares', description = '', photoMo
 
   return (
     <>
-      <Head title={title} description={description} />
-      <div
-        className={clsx('mx-5 my-5', {
-          'lg:my-20 lg:mx-auto lg:max-w-screen-lg': !photoMode,
-        })}
-      >
-        <ThemeToggleButton
-          switchTheme={switchTheme}
-          theme={theme}
-          className={clsx('hidden', { 'lg:block lg:absolute top-5 right-5': !photoMode })}
-        />
-        <header className={clsx({ 'mb-16 lg:absolute': !photoMode, 'max-w-screen-xl mx-auto': photoMode })}>
-          <nav className={clsx('flex justify-between', { 'lg:flex-col': !photoMode })}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {description ? <meta name="description" content={description} /> : null}
+        <link rel="shortcut icon" type="image/x-icon" href="/assets/favicon.ico" />
+      </Head>
+      <div className="mx-5 my-5">
+        <header className="max-w-screen-xl mx-auto">
+          <nav className="flex justify-between">
             <Link href="/">
               <a className="overflow-y-hidden h-6 font-bold no-underline flex-grow transition-colors duration-300 ease-in-out text-black dark:text-neutral hover:text-gray-700 dark:hover:text-gray-400 lg:mb-2">
                 sal olivares
               </a>
             </Link>
-            <ul className={clsx('flex space-x-3', { 'lg:flex-col lg:space-x-0': !photoMode })}>
+            <ul className="flex space-x-3">
               {navLinks.map((navLink) => (
                 <li key={navLink.link}>
                   <Link href={navLink.link}>
@@ -64,14 +59,10 @@ const Container = ({ children, title = 'Sal Olivares', description = '', photoMo
                 </li>
               ))}
             </ul>
-            <ThemeToggleButton
-              switchTheme={switchTheme}
-              theme={theme}
-              className={clsx('self-start ml-4', { 'lg:ml-0 lg:hidden': !photoMode })}
-            />
+            <ThemeToggleButton switchTheme={switchTheme} theme={theme} className="self-start ml-4" />
           </nav>
         </header>
-        <div className={clsx({ 'max-w-screen-sm mx-auto': !photoMode })}>{children}</div>
+        <div>{children}</div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,32 +7,32 @@ import FAV_THINGS from '../utils/favs';
 import { shuffleArray } from '../utils/utils';
 import { Socials } from '../components/Socials';
 
-const Home = () => (
-  <Container title="Sal Olivares" description="Sal Olivares. Software Engineer.">
-    <div className="lg:max-w-md mx-auto">
-      <ContentSection className="">
-        <p>Hi, I&apos;m Sal.</p>
-        <p>
-          I&apos;m a software engineer trying his best to document his experiences. You can usually find me evangelizing
-          others to the wonders of TypeScript.
-        </p>
-        <p className="h-16">
-          I also enjoy{' '}
-          <Typewriter
-            options={{
-              strings: shuffleArray(FAV_THINGS.map((h) => `${h}.`)),
-              autoStart: true,
-              loop: true,
-              skipAddStyles: true,
-              delay: 50,
-              deleteSpeed: 30,
-            }}
-          />
-        </p>
-      </ContentSection>
-      <Socials />
-    </div>
-  </Container>
-);
-
-export default Home;
+export default function Home() {
+  return (
+    <Container title="Sal Olivares" description="Sal Olivares. Software Engineer.">
+      <div className="lg:max-w-md mx-auto">
+        <ContentSection className="">
+          <p>Hi, I&apos;m Sal.</p>
+          <p>
+            I&apos;m a software engineer trying his best to document his experiences. You can usually find me
+            evangelizing others to the wonders of TypeScript.
+          </p>
+          <p className="h-16">
+            I also enjoy{' '}
+            <Typewriter
+              options={{
+                strings: shuffleArray(FAV_THINGS.map((h) => `${h}.`)),
+                autoStart: true,
+                loop: true,
+                skipAddStyles: true,
+                delay: 50,
+                deleteSpeed: 30,
+              }}
+            />
+          </p>
+        </ContentSection>
+        <Socials />
+      </div>
+    </Container>
+  );
+}
