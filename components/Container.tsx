47716d5ab8e65da94b510e59b3e9b7f9771eda8e
Sal Olivares <git@salolivares.com>
2021-02-23 21:31:09 -0800

    more container work

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,14 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/require-default-props */
 import React, { ReactNode, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { ThemeToggleButton } from './ThemeToggleButton';
 
 type WrapperProps = {
   children?: ReactNode;
-  title?: string;
-  description?: string;
+  [index: string]: any;
 };
 
 const navLinks = [
@@ -17,7 +19,7 @@ const navLinks = [
   { link: '/photos', name: 'Photos' },
 ];
 
-const Container = ({ children, title = 'Sal Olivares', description = '' }: WrapperProps) => {
+const Container = (props: WrapperProps) => {
   const [isMounted, setIsMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
@@ -31,39 +33,64 @@ const Container = ({ children, title = 'Sal Olivares', description = '' }: Wrapp
     }
   };
 
+  const { children, ...customMeta } = props;
+  const router = useRouter();
+  const meta = {
+    title: 'Sal Olivares',
+    description: 'Software Engineer and TypeScript enthusiast',
+    image: '',
+    type: 'website',
+    ...customMeta,
+  };
+
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:url" content={`https://salolivares.com${router.asPath}`} />
+        <link rel="canonical" href={`https://salolivares.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Sal Olivares" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@0x102c" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {(meta as any).date && <meta property="article:published_time" content={(meta as any).date} />}
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {description ? <meta name="description" content={description} /> : null}
         <link rel="shortcut icon" type="image/x-icon" href="/assets/favicon.ico" />
       </Head>
-      <div className="mx-5 my-5">
-        <header className="max-w-screen-xl mx-auto">
-          <nav className="flex justify-between">
-            <Link href="/">
-              <a className="overflow-y-hidden h-6 font-bold no-underline flex-grow transition-colors duration-300 ease-in-out text-black dark:text-neutral hover:text-gray-700 dark:hover:text-gray-400 lg:mb-2">
-                sal olivares
-              </a>
-            </Link>
-            <ul className="flex space-x-3">
-              {navLinks.map((navLink) => (
-                <li key={navLink.link}>
-                  <Link href={navLink.link}>
-                    <a className="text-black dark:text-neutral transition duration-300 ease-in-out no-underline hover:underline">
-                      {navLink.name}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <ThemeToggleButton switchTheme={switchTheme} theme={theme} className="self-start ml-4" />
-          </nav>
-        </header>
-        <div>{children}</div>
-      </div>
+      <nav className="flex justify-between items-center max-w-4xl w-full mx-auto p-8 my-0 md:my-8">
+        <a href="#skip" className="sr-only focus:not-sr-only">
+          Skip to content
+        </a>
+        <Link href="/">
+          <a className="overflow-y-hidden h-6 font-bold no-underline flex-grow transition-colors duration-300 ease-in-out text-black dark:text-neutral hover:text-gray-700 dark:hover:text-gray-400 lg:mb-2">
+            sal olivares
+          </a>
+        </Link>
+        <ul className="flex sm:space-x-8 space-x-3">
+          {navLinks.map((navLink) => (
+            <li key={navLink.link}>
+              <Link href={navLink.link}>
+                <a className="text-black dark:text-neutral transition duration-300 ease-in-out no-underline hover:underline">
+                  {navLink.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <ThemeToggleButton switchTheme={switchTheme} theme={theme} className="" />
+      </nav>
+      <main id="skip" className="flex flex-col justify-center px-8">
+        {children}
+      </main>
     </>
   );
 };
