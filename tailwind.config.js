ce028cfb717baa093fb76e9fcad8f1c9b9711b82
Sal Olivares <git@salolivares.com>
2021-02-28 15:16:25 -0800

    custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   variants: {},
   plugins: [],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
     backgroundColor: theme => ({
       ...theme('colors'),
       'neutral': '#131516'
