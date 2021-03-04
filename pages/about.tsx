0faa83a06876a2862865fe0adee4d84f988480b8
Sal Olivares <git@salolivares.com>
2021-03-03 21:19:40 -0800

    fix image

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,15 +2,13 @@ import Image from 'next/image';
 import Container from '../components/Container';
 import Content from '../components/Content';
 
+const ASPECT_RATIO = 2048 / 1365;
+
 const AboutPage = () => (
   <Container title="About | Sal Olivares">
     <div className="max-w-2xl mx-auto mb-16">
       <Content>
-        <div className="h-96">
-          <div className="relative w-full h-full">
-            <Image alt="Sal Olivares" src="/static/images/sal.jpg" layout="fill" objectFit="cover" />
-          </div>
-        </div>
+        <Image alt="Sal Olivares" src="/static/images/sal.jpg" height={500} width={500 * ASPECT_RATIO} />
         <p>My name is Sal.</p>
         <p>
           I&apos;m a software engineer with a passion for technology and photography. I love learning about the bleeding
