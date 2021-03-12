a84ba83a3b325c96e3dc4081729051df9b15843e
Sal Olivares <git@salolivares.com>
2021-03-11 20:13:10 -0800

    add wip to projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,13 @@
 import Container from '../components/Container';
-import PageHeader from '../components/PageHeader';
+import Content from '../components/Content';
 
 const ProjectsPage = () => (
   <Container title="Projects | Sal Olivares">
-    <div className="max-w-2xl mx-auto mb-16 w-full">
-      <PageHeader>Projects</PageHeader>
+    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+      <Content>
+        <h1>Projects</h1>
+        <p>Sal's been pretty busy. He'll update this page asap.</p>
+      </Content>
     </div>
   </Container>
 );
