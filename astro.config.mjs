1b86542ce90594648572d90e106afa2b8e60094f
Sal Olivares <git@salolivares.com>
2023-04-05 23:32:09 -0700

    remove mdx, add image integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from "@astrojs/tailwind";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salolivares.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind(), image({ serviceEntryPoint: '@astrojs/image/sharp' })],
 });
