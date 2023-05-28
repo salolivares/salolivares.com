52c60e278ced2d25f5a60730582a78ddbe7d6f40
Sal Olivares <git@salolivares.com>
2023-05-28 02:25:15 -0700

    make fonts work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import prefetch from '@astrojs/prefetch';
+import { readFileSync } from 'node:fs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,8 +21,26 @@ export default defineConfig({
     prefetch(),
   ],
   vite: {
+    plugins: [rawFonts(['.ttf', '.woff'])],
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
   },
 });
+
+// vite plugin to import fonts
+// https://github.com/kevinzunigacuellar/web/blame/main/astro.config.mjs
+function rawFonts(ext) {
+  return {
+    name: 'vite-plugin-raw-fonts',
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
