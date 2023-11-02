42cb55dc902766234873150ace255306e817821e
Sal Olivares <git@salolivares.com>
2023-11-01 23:10:37 -0700

    do not show unpublished things in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,11 @@ export async function get(context) {
     title: config.title,
     description: config.description,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts
+      .filter((post) => post.data.published)
+      .map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      })),
   });
 }
