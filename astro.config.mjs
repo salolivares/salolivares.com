6cb4012cb8b2549012cbcf558f2df0addd83b7b3
Sal Olivares <salolivares@users.noreply.github.com>
2026-04-03 00:47:46 -0700

    Better Fonts (#60)
    fonts + css fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import { readFileSync } from 'node:fs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,26 +16,9 @@ export default defineConfig({
     domains: ['photos.salolivares.com'],
   },
   vite: {
-    plugins: [tailwindcss(), rawFonts(['.ttf', '.woff'])],
+    plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
   },
 });
-
-// vite plugin to import fonts
-// https://github.com/kevinzunigacuellar/web/blame/main/astro.config.mjs
-function rawFonts(ext) {
-  return {
-    name: 'vite-plugin-raw-fonts',
-    transform(_, id) {
-      if (ext.some((e) => id.endsWith(e))) {
-        const buffer = readFileSync(id);
-        return {
-          code: `export default ${JSON.stringify(buffer)}`,
-          map: null,
-        };
-      }
-    },
-  };
-}
